c5cd5bd6bafc095a6d573268b7f58e2f0901d531
Guillaume Leger <guillaume.leger@moesol.com>
2017-04-05 21:27:51 -0700

    updating algorithm interface

--- algorithm/src/main/java/com/test/test/algorithm/Algorithm.java
+++ algorithm/src/main/java/com/test/test/algorithm/Algorithm.java
@@ -1,5 +1,9 @@
 package com.test.test.algorithm;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface Algorithm {
 
+	Optional<List<Edge>> findShortestPath(Graph g, MapVertice from, MapVertice to);
 }
---
+++ algorithm/src/main/java/com/test/test/algorithm/AlgorithmImpl.java
@@ -0,0 +1,12 @@
+package com.test.test.algorithm;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AlgorithmImpl implements Algorithm{
+
+	public Optional<List<Edge>> findShortestPath(Graph g, MapVertice from, MapVertice to) {
+		return null;
+	}
+
+}
